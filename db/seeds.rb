e1f31146e850f56620c1a0a878687dd2f3aee03b
Philip Krause <philip.krause88@yahoo.de>
2022-01-15 18:22:25 +0100

    done almost

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  task = Task.create(title: Faker::Food.dish, details: Faker::Food.description, completed: false)
+  puts "Task #{task.id} created"
+end
